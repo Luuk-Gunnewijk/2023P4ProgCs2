97b1a3f185e5f7eac972cec1a65995fad454fedb
Luuk <35917@ma-web.nl>
2023-05-08 12:08:24 +0200

    Class 1

---
+++ ConsoleMonGame/ConsoleMonGame/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleMonGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class ConsoleMon
+    {
+        internal int health;
+        internal int energy;
+        internal string name;
+    }
+}
